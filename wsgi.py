39016236d6997b2d1910d8cd9b208c0661a49f36
Jesse Whitehouse <jesse@whitehouse.dev>
2019-10-27 14:25:56 -0500

    WIP: Refactored /add endpoint using methodview

--- wsgi.py
+++ wsgi.py
@@ -145,35 +145,81 @@ class VoteResource(MethodView):
 # │                                 │
 # └─────────────────────────────────┘
 
-def create_submission():
-	'''Taken straight from Flask docs'''
 
-	if request.method == 'POST':
-			# check if the post request has the file part
-			if 'file' not in request.files:
-				flash('No file part')
-				return redirect(request.url)
-			file = request.files['file']
-			# if user does not select file, browser also
-			# submit an empty part without filename
-			if file.filename == '':
-				flash('No selected file')
-				return redirect(request.url)
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-				
-			this_submission = Submission()
-			this_submission.contestant=request.form['contestant']
-			this_submission.costume_title=request.form['costume_title']
-			this_submission.photo_path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+class PageSubmitAContestant(MethodView):
 
-			session.add(this_submission)
-			session.commit()
-	else:
+	def get(self):
 		return render_template('index.html')
-	
 
+
+	def post(self):
+		'''Taken straight from Flask docs'''
+
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and self.allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			
+		this_submission = Submission()
+		this_submission.contestant=request.form['contestant']
+		this_submission.costume_title=request.form['costume_title']
+		this_submission.photo_path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+		session.add(this_submission)
+		session.commit()
+
+
+		return self.get()
+	
+	@staticmethod
+	def allowed_file(filename):
+		return '.' in filename and \
+			   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+def election_page():
+
+	submissions = session.query(Submission).order_by(Submission.contestant).all()
+
+	# handle row calculation logic because it's easier to debug here than in Jinja
+
+	submission_count = len(submissions)
+
+	if submission_count % 3 == 0:
+		row_count = submission_count // 3
+	else:
+		row_count = (submission_count // 3) + 1
+
+	grid = []
+
+	for rownum in range(row_count):
+
+		l = []
+		if rownum + 1 == row_count:
+			row_width = submission_count % 3
+		else:
+			row_width = 3
+		
+		for i in range(row_width):
+			l.append(submissions[rownum*3 + i -1])
+
+		grid.append(l)
+
+	
+	return render_template('election.html', grid=grid)
+
+
+def results_page():
+	winners = session.query(Submission, sa.func.count(Vote.id)).outerjoin(Vote).group_by(Submission.id).order_by(sa.func.count(Vote.id).desc()).one()
+	
 
 # ┌─────────────────────────────────┐
 # │       Application Startup       │
@@ -192,16 +238,9 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'BOO'
 
-app.add_url_rule('/add', endpoint='add', view_func=create_submission, methods=["GET", "POST"])
+app.add_url_rule('/', endpoint='index', view_func=election_page)
+app.add_url_rule('/add', endpoint='add', view_func=PageSubmitAContestant.as_view('make_a_submission'))
 
 app.add_url_rule('/submissions', view_func=SubmissionListResource.as_view('submissions'))
 app.add_url_rule('/submissions/<submission_id>', view_func=SubmissionResource.as_view('submission'))
 app.add_url_rule('/vote/<submission_id>', view_func=VoteResource.as_view('votes'))
-
-
-
-
-def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
